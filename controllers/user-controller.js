79baf892406892bc4f4084d092129ab50a8c8cbe
Topash15 <alextopash@gmail.com>
2021-10-23 15:51:56 -0400

    fixed merge issues

--- controllers/user-controller.js
+++ controllers/user-controller.js
@@ -78,34 +78,3 @@ const UserController = {
 };
 
 module.exports = UserController;
-
-    // get all Users
-    getAllUsers(req, res) {
-        User.find()
-            // .populate({
-            //     path: 'thoughts',
-            //     select: '-__v'
-            // })
-            // .select('-__v')
-            .then((dbUserData) => res.json(dbUserData))
-            .catch((err) => {
-                console.log(err)
-                res.status(404).json(err)
-            })
-    },
-    // get user by id
-
-    // create user
-    createUser(req, res) {
-        User.create(req.body)
-          .then((dbUserData) => res.json(dbUserData))
-          .catch((err) => res.status(400).json(err));
-      },
-
-    // update user
-
-    // delete user
-}
-
-module.exports = UserController;
-
