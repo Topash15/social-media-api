9503552253f718c543b94a0a2e442417e835d463
Topash15 <Alextopash@gmail.com>
2021-10-24 12:46:03 -0400

    added validator for user email

--- models/User.js
+++ models/User.js
@@ -13,7 +13,7 @@ const UserSchema = new Schema(
         type: String,
         required: true,
         unique: true,
-        // TODO must be valid email. use mongoose validation
+        match: /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/
     },
     thoughts: [ 
         {
